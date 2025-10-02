34bd77d585ee62e615bd7a3920a7439e2e354daa
TimLMueller <timlucas.mueller@gmail.com>
2025-10-02 18:44:06 +0200

    init frontend

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ]
+}
