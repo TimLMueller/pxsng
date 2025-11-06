12236aa2e33a5b8926a37b219b6330e3d9c32915
s4mmich <sila.schirrmacher@gmail.com>
2025-11-06 13:21:22 +0100

    Installed restx and added dummy controllers

---
+++ backend/Controllers/Course.py
@@ -0,0 +1,34 @@
+from flask_restx import Namespace, Resource, fields
+
+api = Namespace("courses", description="courses related operations")
+
+course_model = api.model(
+    "Courses",
+    {
+        "id": fields.Integer(),
+        "name": fields.String(),
+        "description": fields.String(),
+        "price": fields.Float(),
+    },
+)
+
+COURSES = [
+    {"id": 1, "name": "test1", "description": "testCourse", "price": 3.10},
+    {"id": 2, "name": "test2", "description": "testCourse2", "price": 2.90},
+]
+
+
+@api.route("/")
+class CoursesController(Resource):
+    @api.marshal_list_with(course_model)
+    def get(self):
+        return COURSES
+
+
+@api.route("/<int:id>")
+class CourseController(Resource):
+    def get(self, id):
+        course = next((course for course in COURSES if course["id"] == id), None)
+        if course:
+            return course
+        api.abort(404, f"Course {id} not found")
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,34 @@
 from flask import Flask
+from flask_restx import Api
+from Controllers.Course import api as courses_ns
 
 app = Flask(__name__)
+api = Api(
+    app,
+    version="1.0",
+    title="pxsng backend"
+)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+api.add_namespace(courses_ns, path="/courses")
+# api.init_app(app)
+
+# myCourse = Course("test1", "testCourse", 3.00)
+# courses = [
+#         myCourse
+#     # { 'description': 'salary', 'amount': 5000 }
+# ]
+
+
+# class CourseController:
+#     def get(self):
+#         return courses
+
+
+# @app.route('/incomes', methods=['POST'])
+# def add_income():
+#     courses.append(request.get_json())
+#     return '', 204
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
